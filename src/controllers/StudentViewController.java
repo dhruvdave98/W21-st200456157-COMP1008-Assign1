7cd96689de36196d01e939804fcc7bf3f22613e2
Dhruv Dave <77804059+dhruvdave98@users.noreply.github.com>
2021-03-09 17:49:49 -0500

    assigned a value to labels and listview

--- src/controllers/StudentViewController.java
+++ src/controllers/StudentViewController.java
@@ -1,9 +1,14 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
-public class StudentViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentViewController implements Initializable {
     @FXML
     private Label firstNameLabel;
 
@@ -14,5 +19,13 @@ public class StudentViewController {
     private Label studentNumberLabel;
 
     @FXML
-    private Label favouriteActivitiesLabel;
+    private ListView favouriteActivitiesList;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        firstNameLabel.setText("Dhruv");
+        lastNameLabel.setText("Dave");
+        studentNumberLabel.setText("200456157");
+        favouriteActivitiesList.getItems().addAll("Travelling","Music","Computer Gaming","Watching movies");
+    }
 }
