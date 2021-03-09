4da7e7a8b91def6172c08f321dc5820de5902d7c
Dhruv Dave <77804059+dhruvdave98@users.noreply.github.com>
2021-03-09 14:54:09 -0500

    updated views and controller to set fx:id

---
+++ src/controllers/StudentViewController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StudentViewController {
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Label studentNumberLabel;
+
+    @FXML
+    private Label favouriteActivitiesLabel;
+}
